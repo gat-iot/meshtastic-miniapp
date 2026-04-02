0770e2969d211612830d6147f5e28881274fe091
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 16:43:13 +0800

    Update pages/messages/messages.js

--- pages/messages/messages.js
+++ pages/messages/messages.js
@@ -1,3 +1,5 @@
+var ble = require('../../utils/ble');
+
 Page({
   data: {
     messages: [],
@@ -6,11 +8,15 @@ Page({
   },
 
   onShow: function() {
-    var app = getApp();
     this.setData({
-      isConnected: !!app.globalData.deviceId && app.globalData.isConnected
+      isConnected: ble.isConnected
     });
-    console.log('消息页面 - 连接状态:', this.data.isConnected);
+    
+    // 加载全局消息
+    var app = getApp();
+    if (app.globalData.messages.length !== this.data.messages.length) {
+      this.setData({ messages: app.globalData.messages });
+    }
   },
 
   onInput: function(e) {
@@ -22,24 +28,37 @@ Page({
       wx.showToast({ title: '请输入消息', icon: 'none' });
       return;
     }
-
     if (!this.data.isConnected) {
       wx.showToast({ title: '请先连接设备', icon: 'none' });
       return;
     }
 
-    var msg = {
-      id: Date.now(),
-      type: 'sent',
-      text: this.data.inputText,
-      time: new Date().toLocaleTimeString()
-    };
+    var text = this.data.inputText.trim();
+    var that = this;
 
-    this.setData({
-      messages: this.data.messages.concat(msg),
-      inputText: ''
+    // 发送消息到设备
+    ble.sendText(text, function(result) {
+      var msg = {
+        id: Date.now(),
+        type: 'sent',
+        text: text,
+        time: new Date().toLocaleTimeString()
+      };
+
+      // 添加到列表
+      that.setData({
+        messages: that.data.messages.concat(msg),
+        inputText: ''
+      });
+
+      // 保存到全局
+      getApp().globalData.messages.push(msg);
+
+      if (result.success) {
+        wx.showToast({ title: '已发送', icon: 'success' });
+      } else {
+        wx.showToast({ title: '发送失败', icon: 'none' });
+      }
     });
-
-    wx.showToast({ title: '消息已发送', icon: 'success' });
   }
 });
