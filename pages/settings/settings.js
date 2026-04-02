078cd4591d789dc4f0d7ab9bcfd214945c2e2721
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 17:00:13 +0800

    Update pages/settings/settings.js

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,44 +1,43 @@
+var ble = require('../../utils/ble');
+
 Page({
   data: {
-    isConnected: false,
+    connected: false,
     deviceName: '',
     version: 'v1.0.0'
   },
 
   onShow: function() {
-    var app = getApp();
-    var device = app.globalData.connectedDevice;
     this.setData({
-      isConnected: !!app.globalData.deviceId && app.globalData.isConnected,
-      deviceName: device ? (device.name || device.localName) : ''
+      connected: ble.connected,
+      deviceName: ble.nodeName || ''
     });
-    console.log('设置页面 - 连接状态:', this.data.isConnected);
   },
 
   disconnect: function() {
     var that = this;
     wx.showModal({
       title: '断开连接',
-      content: '确定要断开当前设备吗？',
+      content: '确定要断开吗？',
       success: function(res) {
         if (res.confirm) {
-          var deviceId = getApp().globalData.deviceId;
-          if (deviceId) {
-            wx.closeBLEConnection({
-              deviceId: deviceId,
-              success: function() {
-                getApp().globalData.deviceId = null;
-                getApp().globalData.connectedDevice = null;
-                getApp().globalData.isConnected = false;
-                getApp().globalData.serviceId = null;
-                that.setData({
-                  isConnected: false,
-                  deviceName: ''
-                });
-                wx.showToast({ title: '已断开', icon: 'success' });
-              }
-            });
-          }
+          ble.disconnect(function() {
+            that.setData({ connected: false, deviceName: '' });
+            wx.showToast({ title: '已断开', icon: 'success' });
+          });
+        }
+      }
+    });
+  },
+
+  clearMessages: function() {
+    wx.showModal({
+      title: '清除消息',
+      content: '确定清除所有消息？',
+      success: function(res) {
+        if (res.confirm) {
+          getApp().globalData.messages = [];
+          wx.showToast({ title: '已清除', icon: 'success' });
         }
       }
     });
@@ -46,8 +45,8 @@ Page({
 
   showAbout: function() {
     wx.showModal({
-      title: '关于 Meshtastic',
-      content: 'Meshtastic 微信小程序\n版本: 1.0.0\n\n用于连接和管理 Meshtastic 设备',
+      title: '关于',
+      content: 'Meshtastic 微信小程序\n版本: 1.0.0\n\n离网通信客户端\n支持 BLE 连接 Meshtastic 设备',
       showCancel: false
     });
   }
