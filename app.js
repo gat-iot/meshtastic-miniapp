264c405e694a161ff8222daa40db9d2cd5d164a9
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 15:21:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ App({
   globalData: {
     connectedDevice: null,
     deviceId: null,
+    isConnected: false,
+    serviceId: null,
     messages: []
   },
-  onLaunch() {
+
+  onLaunch: function() {
     console.log('Meshtastic MiniApp 启动');
+  },
+
+  onShow: function() {
+    console.log('App 显示');
   }
 });
