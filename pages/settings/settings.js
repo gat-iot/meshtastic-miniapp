5f827d33c8ddb71f488a257438de85d6f3bfcddf
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 15:21:29 +0800

    Update pages/settings/settings.js

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,17 +1,54 @@
 Page({
-  data: { connected: false, deviceName: '' },
-  onShow() {
-    const app = getApp();
-    this.setData({ connected: !!app.globalData.deviceId, deviceName: app.globalData.connectedDevice?.name || '' });
+  data: {
+    isConnected: false,
+    deviceName: '',
+    version: 'v1.0.0'
   },
-  disconnect() {
-    if (getApp().globalData.deviceId) {
-      wx.closeBLEConnection({ deviceId: getApp().globalData.deviceId });
-    }
-    getApp().globalData.deviceId = null;
-    this.setData({ connected: false });
+
+  onShow: function() {
+    var app = getApp();
+    var device = app.globalData.connectedDevice;
+    this.setData({
+      isConnected: !!app.globalData.deviceId && app.globalData.isConnected,
+      deviceName: device ? (device.name || device.localName) : ''
+    });
+    console.log('设置页面 - 连接状态:', this.data.isConnected);
   },
-  showAbout() {
-    wx.showModal({ title: '关于', content: 'Meshtastic 微信小程序 v1.0', showCancel: false });
+
+  disconnect: function() {
+    var that = this;
+    wx.showModal({
+      title: '断开连接',
+      content: '确定要断开当前设备吗？',
+      success: function(res) {
+        if (res.confirm) {
+          var deviceId = getApp().globalData.deviceId;
+          if (deviceId) {
+            wx.closeBLEConnection({
+              deviceId: deviceId,
+              success: function() {
+                getApp().globalData.deviceId = null;
+                getApp().globalData.connectedDevice = null;
+                getApp().globalData.isConnected = false;
+                getApp().globalData.serviceId = null;
+                that.setData({
+                  isConnected: false,
+                  deviceName: ''
+                });
+                wx.showToast({ title: '已断开', icon: 'success' });
+              }
+            });
+          }
+        }
+      }
+    });
+  },
+
+  showAbout: function() {
+    wx.showModal({
+      title: '关于 Meshtastic',
+      content: 'Meshtastic 微信小程序\n版本: 1.0.0\n\n用于连接和管理 Meshtastic 设备',
+      showCancel: false
+    });
   }
 });
