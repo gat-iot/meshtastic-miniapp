5733c4034cfd6d79f3e203aeaab8778615d12fe8
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 16:59:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+var ble = require('./utils/ble');
+
 App({
   globalData: {
-    connectedDevice: null,
-    deviceId: null,
-    isConnected: false,
     messages: []
   },
+
   onLaunch: function() {
     console.log('Meshtastic MiniApp 启动');
+    // 初始化 BLE 监听
+    ble.init();
+    
+    // 设置消息回调
+    ble.onMessage = function(parsed) {
+      if (parsed.type === 'packet' && parsed.data.text) {
+        this.globalData.messages.push({
+          id: Date.now(),
+          type: 'received',
+          text: parsed.data.text,
+          time: new Date().toLocaleTimeString(),
+          from: parsed.data.from
+        });
+      }
+    }.bind(this);
   }
 });
