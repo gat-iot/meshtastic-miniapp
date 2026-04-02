594929b4d1846d6088a1cd630c0ce60faa886cb6
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 15:21:25 +0800

    Update pages/channels/channels.js

--- pages/channels/channels.js
+++ pages/channels/channels.js
@@ -1,13 +1,29 @@
 Page({
-  data: { channels: [
-    { index: 0, name: 'LongFast', enabled: true },
-    { index: 1, name: 'MediumFast', enabled: false },
-    { index: 2, name: 'ShortTurbo', enabled: false }
-  ]},
-  toggleChannel(e) {
-    const index = e.currentTarget.dataset.index;
-    const channels = this.data.channels;
+  data: {
+    channels: [
+      { index: 0, name: 'LongFast', enabled: true, desc: '远距离、低速率' },
+      { index: 1, name: 'MediumFast', enabled: false, desc: '中等距离和速率' },
+      { index: 2, name: 'ShortTurbo', enabled: false, desc: '短距离、高速率' },
+      { index: 3, name: '自定义', enabled: false, desc: '用户自定义' }
+    ],
+    isConnected: false
+  },
+
+  onShow: function() {
+    var app = getApp();
+    this.setData({
+      isConnected: !!app.globalData.deviceId && app.globalData.isConnected
+    });
+  },
+
+  toggleChannel: function(e) {
+    var index = e.currentTarget.dataset.index;
+    var channels = this.data.channels;
     channels[index].enabled = !channels[index].enabled;
-    this.setData({ channels });
+    this.setData({ channels: channels });
+    wx.showToast({
+      title: channels[index].enabled ? '已启用' : '已禁用',
+      icon: 'success'
+    });
   }
 });
