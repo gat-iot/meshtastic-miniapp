bb543341d1b964b9dc5dac6c6832192f0b4c637b
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 17:29:53 +0800

    Update pages/devices/devices.js

--- pages/devices/devices.js
+++ pages/devices/devices.js
@@ -6,7 +6,7 @@ Page({
     devices: [],
     connected: false,
     deviceName: '',
-    error: ''
+    debugLog: ''
   },
 
   onShow: function() {
@@ -14,48 +14,65 @@ Page({
       connected: ble.connected,
       deviceName: ble.nodeName || ''
     });
+    
+    // 启用通知
+    if (ble.connected) {
+      ble.enableNotify();
+    }
+  },
+
+  log: function(msg) {
+    console.log(msg);
+    this.setData({
+      debugLog: this.data.debugLog + msg + '\n'
+    });
   },
 
   scanDevices: function() {
     var that = this;
-    that.setData({ scanning: true, error: '', devices: [] });
+    that.setData({ scanning: true, devices: [], debugLog: '' });
+    that.log('开始扫描...');
 
     wx.openBluetoothAdapter({
       success: function() {
+        that.log('蓝牙初始化成功');
+        
         wx.startBluetoothDevicesDiscovery({
           allowDuplicatesKey: false,
           success: function() {
-            wx.showToast({ title: '扫描中...', icon: 'none' });
-
+            that.log('正在扫描设备...');
+            
             setTimeout(function() {
               wx.getBluetoothDevices({
                 success: function(res) {
-                  console.log('[SCAN] 发现设备:', res.devices.length);
+                  that.log('发现 ' + res.devices.length + ' 个设备');
                   that.setData({
                     devices: res.devices,
                     scanning: false
                   });
                   wx.stopBluetoothDevicesDiscovery();
                 },
-                fail: function() {
-                  that.setData({ scanning: false, error: '获取设备列表失败' });
-                  wx.stopBluetoothDevicesDiscovery();
+                fail: function(err) {
+                  that.log('获取设备失败: ' + err.errMsg);
+                  that.setData({ scanning: false });
                 }
               });
             }, 8000);
           },
-          fail: function() {
-            that.setData({ scanning: false, error: '扫描启动失败' });
+          fail: function(err) {
+            that.log('扫描失败: ' + err.errMsg);
+            that.setData({ scanning: false });
           }
         });
       },
       fail: function(err) {
-        that.setData({ scanning: false });
+        that.log('蓝牙初始化失败');
         wx.showModal({
-          title: '蓝牙错误',
-          content: '请开启手机蓝牙后重试',
+          title: '错误',
+          content: '请开启手机蓝牙',
           showCancel: false
         });
+        that.setData({ scanning: false });
       }
     });
   },
@@ -64,20 +81,26 @@ Page({
     var that = this;
     var deviceId = e.currentTarget.dataset.id;
     var device = this.data.devices.find(function(d) { return d.deviceId === deviceId; });
-
+    
     wx.stopBluetoothDevicesDiscovery();
-
+    that.setData({ debugLog: '' });
+    that.log('连接设备: ' + (device.name || device.localName || deviceId));
+    
     ble.connect(deviceId, function(success, err) {
       if (success) {
         ble.nodeName = device.name || device.localName || 'Meshtastic';
         that.setData({
           connected: true,
-          deviceName: ble.nodeName,
-          devices: []
+          deviceName: ble.nodeName
         });
+        that.log('连接成功!');
+        
+        // 启用通知
+        ble.enableNotify();
+        
         wx.showToast({ title: '已连接', icon: 'success' });
       } else {
-        that.setData({ error: err || '连接失败' });
+        that.log('连接失败: ' + err);
         wx.showToast({ title: '连接失败', icon: 'none' });
       }
     });
@@ -91,7 +114,11 @@ Page({
       success: function(res) {
         if (res.confirm) {
           ble.disconnect(function() {
-            that.setData({ connected: false, deviceName: '' });
+            that.setData({
+              connected: false,
+              deviceName: '',
+              debugLog: ''
+            });
             wx.showToast({ title: '已断开', icon: 'success' });
           });
         }
