12fb04eb9d029f47c6ad576b4e2d39eb93064a2c
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 15:21:22 +0800

    Update pages/devices/devices.js

--- pages/devices/devices.js
+++ pages/devices/devices.js
@@ -1,7 +1,20 @@
 Page({
-  data: { connected: false, deviceName: '' },
-  onShow() {
-    const app = getApp();
-    this.setData({ connected: !!app.globalData.deviceId, deviceName: app.globalData.connectedDevice?.name || '' });
+  data: {
+    isConnected: false,
+    deviceName: '',
+    deviceId: '',
+    rssi: 0
+  },
+
+  onShow: function() {
+    var app = getApp();
+    var device = app.globalData.connectedDevice;
+    this.setData({
+      isConnected: !!app.globalData.deviceId && app.globalData.isConnected,
+      deviceName: device ? (device.name || device.localName) : '',
+      deviceId: app.globalData.deviceId || '',
+      rssi: device ? device.RSSI : 0
+    });
+    console.log('设备页面 - 连接状态:', this.data.isConnected);
   }
 });
