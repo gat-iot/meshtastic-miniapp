747f8ea3541d7767135187d1db02462050f1bbca
GAT-IoT <farce.chen@gat-iot.com>
2026-04-02 18:00:17 +0800

    Update pages/channels/channels.js

--- pages/channels/channels.js
+++ pages/channels/channels.js
@@ -3,17 +3,14 @@ Page({
     channels: [
       { index: 0, name: 'LongFast', enabled: true, desc: '远距离、低速率' },
       { index: 1, name: 'MediumFast', enabled: false, desc: '中等距离和速率' },
-      { index: 2, name: 'ShortTurbo', enabled: false, desc: '短距离、高速率' },
-      { index: 3, name: '自定义', enabled: false, desc: '用户自定义' }
+      { index: 2, name: 'ShortTurbo', enabled: false, desc: '短距离、高速率' }
     ],
-    isConnected: false
+    connected: false
   },
 
   onShow: function() {
-    var app = getApp();
-    this.setData({
-      isConnected: !!app.globalData.deviceId && app.globalData.isConnected
-    });
+    var ble = require('../../utils/ble');
+    this.setData({ connected: ble.connected });
   },
 
   toggleChannel: function(e) {
